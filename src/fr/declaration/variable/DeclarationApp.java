9e60f5a2a428266d1bd49475205cbbd07096b250
Tubeuf <charles.tubeuf@hotmail.fr>
2021-04-22 14:22:02 +0200

    test premier comment

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,25 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		
+		
+		byte varByte = 1;
+		short varShort = 2;
+		int varInt = 3;
+		long varLong = 4L;
+		float varFloat = 5.1f;
+		double varDouble = 6.2d;
+		char varChar = 'a';
+		boolean varBoolean = true; 
+		String varString = "Bonjour Richard";
+		
+		System.out.println(varString); // affichage d'une variable 
+		System.out.println("littéral"); // affichage d'un littéral  
+		
+		
+
+	}
+
+}
